7fd6ff2bf6dc81d2de2f7df49fb53c0136784db9
chaturs <chaturs@miamioh.edu>
2025-10-21 14:30:00 -0400

    Expose ReAct prompt templates

--- src/llmath/prompts/__init__.py
+++ src/llmath/prompts/__init__.py
@@ -1,6 +1,11 @@
 """Prompts module for LLMath - prompt templates and orchestration."""
 
 from .templates import SYSTEM_HEADER, ANSWER_INSTRUCTIONS, BASELINE_PROMPT_TEMPLATE
+from .react_templates import (
+    REACT_SYSTEM_PROMPT,
+    build_react_system_prompt,
+    build_react_user_prompt,
+)
 from .builder import build_math_prompt, build_baseline_prompt
 from .orchestrator import ToolOrchestrator, OrchestratorResult, create_orchestrator
 
@@ -8,6 +13,9 @@ __all__ = [
     "SYSTEM_HEADER",
     "ANSWER_INSTRUCTIONS",
     "BASELINE_PROMPT_TEMPLATE",
+    "REACT_SYSTEM_PROMPT",
+    "build_react_system_prompt",
+    "build_react_user_prompt",
     "build_math_prompt",
     "build_baseline_prompt",
     "ToolOrchestrator",
